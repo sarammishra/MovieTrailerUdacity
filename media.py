1fbb1aaaa64f0e7b5b3e3ec3aa943744e8a5e9f1
SMishra <36138525+sarammishra@users.noreply.github.com>
2018-12-20 15:55:05 -0600

    Update media.py

--- media.py
+++ media.py
@@ -6,6 +6,15 @@ import webbrowser
 # Store movie info within this class
 
 class movie():
+    """This class creates CHARS required for all movies
+        Attributes:
+        attr1 (str): title of movie.
+        attr2 (str): Quick Summary of Movie
+        attr3 (hyperlink): Poster Link to creative commons image 
+        attr4 (hyperlink): Trailer Link to youtube Trailer
+        
+        """
+    
     def __init__(self, movieTitle, movieStory, poster, trailer):
         self.title = movieTitle
         self.story = movieStory
